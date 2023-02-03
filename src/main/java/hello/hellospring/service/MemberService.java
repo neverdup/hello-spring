9be83f997eff34086de329f143e342a7c52f138d
Jaewon Yang <neverdup@gmail.com>
2023-02-03 09:28:01 +0900

    AOP 추가

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
@@ -23,24 +24,16 @@ public class MemberService {
      */
     public Long join(Member member) {
         // 같은 이름이 있는 회원은 안됨
-
-        long start = System.currentTimeMillis();
-
-        try {
-            validateDuplicateMember(member); // 중복 회원 검증
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName((member.getName()))
-                .ifPresent(m -> { throw new IllegalStateException("이미 존재하는 회원입니다.");
-        });
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
     }
 
     /**
